9b9e8edb5bd141de7d172d6d4605d165fe871cb4
PanMan <146111571+jinfengP@users.noreply.github.com>
2025-05-14 13:20:23 -0400

    Add files via upload

--- thethgegre.py
+++ thethgegre.py
@@ -10,23 +10,36 @@ def connectToServer():
     return connection
 
 def askInput():
-    id = input("Enter Student ID: ")
-    query = "call boowomp("+id+")"
-    return query
-
-def yummers():
     connection = connectToServer()
-    query = askInput()
-    cursor = connection.cursor()
+    role = input("Login as Teacher [1] or Login as Student [2]: ")
+    if int(role)==1:
+        tid = input("Enter your Teacher ID: ")
+        teacherSignIn(tid,connection)
+    elif int(role)==2:
+        sid = input("Enter your Student ID: ")
+        studentSignIn(sid,connection)
+
+
+def teacherSignIn(id,c): #WORK IN PROGRESS
+    cursor = c.cursor()
+    cursor.close()
+    c.close()
+
+def studentSignIn(id,c):
+    cursor = c.cursor()
+    query = 'call boowomp('+id+')'
     cursor.execute(query)
-    for row in cursor:
+    printStudentSchedule(cursor)
+    cursor.close()
+    c.close()
+
+
+def printStudentSchedule(list):
+    for row in list:
         print("Period: " + str(row[3]))
         print("Course: " + str(row[1]))
         print("Room:" + str(row[2]))
         print("Teacher: " + str(row[4])+ " " + str(row[5]) + "\n")
-    cursor.close()
-    connection.close()
 
 
-
-yummers()
+askInput()
